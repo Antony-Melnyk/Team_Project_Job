c3bb9831ebeceb2cb2de090ae26ebe3a369cc9a5
user <user@A26COMP12>
2018-07-03 16:57:52 +0300

    Added MainJobWindow

---
+++ WpfApp1/WpfApp1/MainJobWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainJobWindow.xaml
+    /// </summary>
+    public partial class MainJobWindow : Window
+    {
+        public MainJobWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.ShowDialog();
+        }
+    }
+}
