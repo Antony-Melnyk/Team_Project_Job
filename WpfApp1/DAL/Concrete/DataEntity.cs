6796ad3e6e8b58162400542d6ec82c36b7473967
melnyk.i.anton <melnyk.i.anton@outlook.com>
2018-07-03 16:59:18 +0300

    s1

--- WpfApp1/DAL/Concrete/DataEntity.cs
+++ WpfApp1/DAL/Concrete/DataEntity.cs
@@ -37,6 +37,36 @@ namespace DAL.Concrete
         public virtual ICollection<Job> Jobs { get; set; }
     }
 
+    public class Employer
+    {
+        public Employer()
+        {
+            Jobs = new List<Job>();
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string SName { get; set; }
+        [Required]
+        public string TName { get; set; }
+        [Required]
+        public string NickName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string ImgPath { get; set; }
+        public virtual ICollection<Job> Jobs { get; set; }
+    }
+
     public class Job
     {
         public int Id { get; set; }
@@ -50,11 +80,16 @@ namespace DAL.Concrete
         public int Salary { get; set; }
         [Required]
         public string City { get; set; }
-        public string ImgPath { get; set; }
 
+        public string ImgPath { get; set; }
+        [Required]
+        public string NumberOfEmployees { get; set; }
         [Required]
         public string Info { get; set; }
 
+        [Required]
+        public string YourPosition { get; set; }
+        
         [Required]
         public User User { get; set; }
 
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using BLL;
 namespace WpfApp1
 {
     /// <summary>
@@ -27,7 +27,16 @@ namespace WpfApp1
 
         private void ButtonSignIn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (Operations.SignIn(NickName_Text.Text, Password_Text.Password) == true)
+            {
+                Close();
+                MainWindow mw = new MainWindow();
+                mw..Content = NickName_Text.Text;
+                mw.Show();
+                Application.Current.Windows[0].Close();
+            }
+            else
+                MessageBox.Show("Error. Check your login and password", "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
 
         private void Hyperlink_Click(object sender, RoutedEventArgs e)
